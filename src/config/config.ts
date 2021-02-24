eaf967cbb5527cccfdb2af63036ca7992b050950
Dhruvil Shah <dhruvil7797@gmail.com>
2021-02-24 02:47:02 -0500

    PORT Configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -35,7 +35,7 @@ const testConfig = {
 }
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.PORT || 3000;
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
